0e7cdddb2c05b14f0fe0af4487d590cb965c6e7a
Esteban <esteban.schwarzbeck.f@gmail.com>
2025-05-13 07:54:38 -0700

    Receptions works, but still needs to improve a bit. Will conitnue work and come back later, will add comments in logic in next commit

--- routers/db/receptions.py
+++ routers/db/receptions.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 from validations import RecievedDelivery
 from sqlalchemy.orm import Session
-from sqlalchemy import select, func, label
-from database import metadata, get_db
+from sqlalchemy import select, func, label, update
+from database import get_db
 from typing import List, Annotated
 import models
 from models import reception_id, audit_id
@@ -23,6 +23,7 @@ async def package_recieved(deliveries: List[RecievedDelivery], db:db_dependency)
     if deliveries is None:
         raise HTTPException(status_code=400, detail="No order information provided.")
     
+    # Set our variables to save and do the update
     delivery_db = []
     audits_db = []
     packages_to_return = []
@@ -33,7 +34,6 @@ async def package_recieved(deliveries: List[RecievedDelivery], db:db_dependency)
             select(
                 models.Orders.order_id,
                 models.Orders.product_id,
-                models.Orders.item_no,
                 func.sum(models.Orders.boxes_ordered).label('total_boxes_order')
                 ).where(
                     models.Orders.order_id == delivery.order_id,
@@ -78,8 +78,11 @@ async def package_recieved(deliveries: List[RecievedDelivery], db:db_dependency)
             packages_to_return.append(delivery)
             continue
         else:
-            order_quantity = int(sum([order[3] for order in order_db if order.order_id == delivery.order_id]))
+            # Need to get the order quantity from the orders
+            order_quantity = int(sum([order[2] for order in order_db if order.order_id == delivery.order_id]))
+            # Know how much we have recieved
             recieved_quantity = int(sum([recieved[2] for recieved in recieved_db]))
+            # What we have would be if we accept all the delivery the amount of packages
             total_to_be_recieved = recieved_quantity + delivery.quantity_recieved # What would be recieved if accept ALL the delivery + what we have already accepted
             pending_to_recieve = order_quantity - recieved_quantity # What is pending to recieve in the current order
 
@@ -102,15 +105,38 @@ async def package_recieved(deliveries: List[RecievedDelivery], db:db_dependency)
                     package_quality=choices(['good', 'bad'], weights=[0.97,0.03], k=1)[0]
                     )
                 )
-
+            order_db = db.execute(select(
+                    models.Orders.order_id,
+                    models.Orders.product_id,
+                    models.Orders.item_no,
+                    models.Orders.boxes_ordered
+                    ).where(
+                        models.Orders.order_id == delivery.order_id,
+                        models.Orders.product_id == delivery.product_id,
+                        models.Orders.order_status == 'confirmed'
+                    ).order_by(
+                        models.Orders.boxes_ordered
+                    )
+                ).all()
             for order in order_db:
                 if order[3] <= total_to_be_recieved:
-                    order.order_filled_date = datetime.now()
-                    order.order_status = 'filled'
-                    total_to_be_recieved =- order.boxes_ordered
-                    db.commit()
+                    db.execute(
+                        update(
+                            models.Orders
+                        ).where(
+                            models.Orders.order_id == order[0],
+                            models.Orders.product_id == order[1],
+                            models.Orders.item_no == order[2]
+                        ).values(
+                            order_filled_date=datetime.now(),
+                            order_status='filled'
+                        )
+                    )
+                    #db.commit()
+                    total_to_be_recieved =- order[3]
                 elif total_to_be_recieved <= 0:
                     break
+            
             continue
 
         elif order_quantity <= total_to_be_recieved:
@@ -129,9 +155,45 @@ async def package_recieved(deliveries: List[RecievedDelivery], db:db_dependency)
                     package_quality=choices(['good', 'bad'], weights=[0.97,0.03], k=1)[0]
                     )
                 )
+            
+            #Return the packages that are not for the order
             packages_to_return.append(delivery.quantity_recieved - pending_to_recieve)
             
-            
+            recieved_quantity + pending_to_recieve
+
+            order_db = db.execute(select(
+                models.Orders.order_id,
+                models.Orders.product_id,
+                models.Orders.item_no,
+                models.Orders.boxes_ordered
+                ).where(
+                    models.Orders.order_id == delivery.order_id,
+                    models.Orders.product_id == delivery.product_id,
+                    models.Orders.order_status == 'confirmed'
+                ).order_by(
+                    models.Orders.boxes_ordered
+                )
+            ).all()
+
+            for order in order_db:
+                if order[3] <= total_to_be_recieved:
+                    db.execute(
+                        update(
+                        models.Orders
+                        ).where(
+                            models.Orders.order_id == order[0],
+                            models.Orders.product_id == order[1],
+                            models.Orders.item_no == order[2]
+                        ).values(
+                            order_filled_date=datetime.now(),
+                            order_status='filled',
+                            last_updated=datetime.now()
+                        )
+                    )
+                    # db.commit()
+                    total_to_be_recieved =- order[3]
+                elif total_to_be_recieved <= 0:
+                    break
             continue
 
     db.add_all(delivery_db)
